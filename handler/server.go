b89efb2b7259c116bb4752a419804ab350e65e4a
Aarti <aarti.sd@gmail.com>
2017-06-06 15:20:07 -0400

    Added new apis for revised complaint manager

--- handler/server.go
+++ handler/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/mux"
 	"complaintData/complaintMgr"
 	"encoding/json"
+	"complaintData/complaintMgrR"
 )
 
 func GetAllComplaintsHandler(list complaintMgr.ComplaintList, w http.ResponseWriter) {
@@ -30,6 +31,28 @@ func GetAllComplaintsHandler(list complaintMgr.ComplaintList, w http.ResponseWri
 }
 
 
+func GetAllComplaintsHandlerR(list complaintMgrR.ComplaintListR, w http.ResponseWriter) {
+
+	if len(list.Complaints) == 0 {
+		out := []byte("No results for this area code")
+		w.Write(out)
+		return
+	}
+	out, err := json.Marshal(list)
+	w.Header().Add("Content-type", "application/json")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		out = []byte("Unknown app error")
+		w.Write(out)
+
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+	}
+
+}
+
 
 func ComplaintHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -42,4 +65,17 @@ func ComplaintHandler(w http.ResponseWriter, r *http.Request) {
 		list = complaintMgr.GetComplaintsByAreaCodeHandler(vars["area"])
 	}
 	GetAllComplaintsHandler(list, w)
+}
+
+func ComplaintHandlerR(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	var list complaintMgrR.ComplaintListR
+	if vars["area"] == "all" {
+		list = complaintMgrR.GetAllComplaintsR()
+
+	} else {
+		list = complaintMgrR.GetComplaintsByAreaCodeHandlerR(vars["area"])
+	}
+	GetAllComplaintsHandlerR(list, w)
 }
